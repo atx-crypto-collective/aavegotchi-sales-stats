494abb33ccd4d1ec02fb3ca2a20c8f63379fb316
Zak Angelle <zaknotzak@gmail.com>
2021-08-31 00:51:33 -0500

    use top-level await

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,16 @@
 import { getRecentClosedPortalSalesStats } from './stats';
 
-(async () => {
-  const {
-    numberOfSales,
-    lowestSalePrice,
-    highestSalePrice,
-    medianSalePrice,
-    averageSalePrice,
-    timeframe,
-  } = await getRecentClosedPortalSalesStats();
+const {
+  numberOfSales,
+  lowestSalePrice,
+  highestSalePrice,
+  medianSalePrice,
+  averageSalePrice,
+  timeframe,
+} = await getRecentClosedPortalSalesStats();
 
-  console.log(`${numberOfSales} closed gotchi portals sold in the last ${timeframe}\n`);
-  console.log(`average price | ${averageSalePrice.toLocaleString('en-US')} $GHST`);
-  console.log(`median price  | ${medianSalePrice.toLocaleString('en-US')} $GHST`);
-  console.log(`lowest price  | ${lowestSalePrice.toLocaleString('en-US')} $GHST`);
-  console.log(`highest price | ${highestSalePrice.toLocaleString('en-US')} $GHST`);
-})();
+console.log(`${numberOfSales} closed gotchi portals sold in the last ${timeframe}\n`);
+console.log(`average price | ${averageSalePrice.toLocaleString('en-US')} $GHST`);
+console.log(`median price  | ${medianSalePrice.toLocaleString('en-US')} $GHST`);
+console.log(`lowest price  | ${lowestSalePrice.toLocaleString('en-US')} $GHST`);
+console.log(`highest price | ${highestSalePrice.toLocaleString('en-US')} $GHST`);
